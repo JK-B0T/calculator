c6808eccac9a3d19e5f0954c02fab22314df9864
JK-B0T <JK-B0T@users.noreply.github.com>
2024-03-22 11:33:07 +0100

    Add UI functionality

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # calculator
-This is the final project ot The Odin Project foundations course, where I am gonna use all the things I have learned to build a calculator web app
-so I can test and prove my skills.
+This is the final project ot The Odin Project foundations course, where I am gonna use all the things I have learned to build a calculator web app so I can test and prove my skills.
--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let historyArr = [""];
+const buttons = document.querySelector("#calculatorBtnsContainer");
+const input = document.querySelector("#calculatorBtnsContainer input");
+buttons.addEventListener("click", getInput, false);
+
 function add(firstNum, SecondNum) {
     return firstNum + SecondNum;
 }
@@ -14,6 +19,78 @@ function divide(firstNum, SecondNum) {
     return firstNum / SecondNum;
 }
 
-function operate(input) {
-    console.log(input);
+function undoLastInput() {
+    if (historyArr.length > 1) {
+        input.value = historyArr[historyArr.length - 2];
+        historyArr.pop();
+    }
+    console.log(historyArr.pop(), historyArr);
+}
+
+function operate() {
+    console.log(input.value);
+}
+
+function getInput(event) {
+    if (event.target.tagName === "BUTTON") {
+        console.log(event.target.id);
+        switch (event.target.id) {
+            case "btn0":
+                input.value += "0";
+                break;
+            case "btn1":
+                input.value += "1";
+                break;
+            case "btn2":
+                input.value += "2";
+                break;
+            case "btn3":
+                input.value += "3";
+                break;
+            case "btn4":
+                input.value += "4";
+                break;
+            case "btn5":
+                input.value += "5";
+                break;
+            case "btn6":
+                input.value += "6";
+                break;
+            case "btn7":
+                input.value += "7";
+                break;
+            case "btn8":
+                input.value += "8";
+                break;
+            case "btn9":
+                input.value += "9";
+                break;
+            case "btnAdd":
+                input.value += " + ";
+                break;
+            case "btnSustract":
+                input.value += " - ";
+                break;
+            case "btnMultiply":
+                input.value += " x ";
+                break;
+            case "btnDivide":
+                input.value += " / ";
+                break;
+            case "btnFloat":
+                input.value += ".";
+                break;
+            case "btnUndo":
+                undoLastInput();
+                break;
+            case "btnClear":
+                input.value = "";
+                break;
+            case "btnOperate":
+                operate();
+                break;
+        }
+        input.scrollLeft = input.scrollWidth - input.clientWidth;
+        historyArr.push(input.value);
+    }
 }
