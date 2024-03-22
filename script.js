3835679c0aa345ad7d06fd19918a48e22543446b
JK-B0T <JK-B0T@users.noreply.github.com>
2024-03-22 10:20:03 +0100

    Create basic math operation functions

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(firstNum, SecondNum) {
+    return firstNum + SecondNum;
+}
+
+function sustract(firstNum, SecondNum) {
+    return firstNum - SecondNum;
+}
+
+function multiply(firstNum, SecondNum) {
+    return firstNum * SecondNum;
+}
+
+function divide(firstNum, SecondNum) {
+    return firstNum / SecondNum;
+}
+
+function operate(input) {
+    console.log(input);
+}
